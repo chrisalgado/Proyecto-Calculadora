c419654e3671ae70e741aa54bc7293422111c019
Christopher Alexander Salgado <cristopher.salgado1997@gmail.com>
2024-10-09 18:18:20 -0600

    Calculadora proyecto terminado

---
+++ index.js
@@ -0,0 +1,63 @@
+let display = document.getElementById('mostrar-resultado');
+let operando1 = '';
+let operando2 = '';
+let operador = '';
+let allowMultipleOperators = false;
+
+function agregarNumero(numero) {
+    if (allowMultipleOperators) {
+        operando2 += numero;
+        display.value += numero;
+    } else {
+        display.value += numero;
+    }
+}
+
+function operar(op) {
+    if (!allowMultipleOperators) {
+        operando1 = display.value;
+        operador = op;
+        display.value = '';
+        allowMultipleOperators = true;
+    }
+}
+
+function calcular() {
+    if (operando2 !== '') {
+        let resultado;
+
+        switch (operador) {
+            case '+':
+                resultado = parseFloat(operando1) + parseFloat(operando2);
+                break;
+            case '-':
+                resultado = parseFloat(operando1) - parseFloat(operando2);   
+
+                break;
+            case '*':
+                resultado = parseFloat(operando1) * parseFloat(operando2);
+                break;
+            case '/':
+                resultado = parseFloat(operando1) / parseFloat(operando2);
+                break;   
+
+            default:
+                // Handle invalid operator
+                resultado = "Error: Invalid operator";
+        }
+
+        display.value = resultado;
+        operando1 = resultado;
+        operando2 = '';
+        operador = '';
+        allowMultipleOperators = false;
+    }
+}
+
+function borrar() {
+    display.value = '';
+    operando1 = '';
+    operando2 = '';
+    operador = '';
+    allowMultipleOperators = false;
+}
